d94adc72a3334db0d155130750fdfe0363a46eeb
siva <gnanashekar045@gmail.com>
2024-11-30 15:10:29 +0530

    search button update

--- e-Mart/src/components/Navebar.jsx
+++ e-Mart/src/components/Navebar.jsx
@@ -12,6 +12,12 @@ const Navebar = () => {
                         <img src={Logo} alt="Logo" />
                     </a>
                 </div>
+                {/* search bar and order button */}
+                <div>
+                    <div>
+                        <input type="text" placeholder='search' className='w-[200px] sm:w-[200px] group-hover:w-[300px]' />
+                    </div>
+                </div>
             </div>
         </div>
         {/* LowerNaveBar */}
